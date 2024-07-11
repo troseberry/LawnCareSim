5f2f4add8032617b584d6daec4e19dfc95aaa12f
troseberry <tayroseberry@gmail.com>
2024-07-10 23:10:21 -0700

    Got mower movement working
    - Also removed old methods from movement controller being used in another project

--- Assets/Scripts/LawnCareSim/Player/MovementController.cs
+++ Assets/Scripts/LawnCareSim/Player/MovementController.cs
@@ -12,12 +12,11 @@ namespace LawnCareSim.Player
         public static MovementController Instance;
 
         [SerializeField] private float _movementSpeed = 10f;
-        [SerializeField] private float _smoothTurnTime = 0.1f;
+        [SerializeField] private float _rotationSpeed = 3.0f;
 
-        private CharacterController _characterController;
-        private GameObject _playerMesh;
-        private Transform _leftTurn;
-        private Transform _rightTurn;
+        private Rigidbody _rigidbody;
+        private GameObject _leftTurnPivot;
+        private GameObject _rightTurnPivot;
 
         private void Awake()
         {
@@ -26,26 +25,32 @@ namespace LawnCareSim.Player
 
         private void Start()
         {
-            _characterController = GetComponent<CharacterController>();
+            _rigidbody = GetComponent<Rigidbody>();
+            _leftTurnPivot = GameObject.Find("LeftTurnAxis");
+            _rightTurnPivot = GameObject.Find("RightTurnAxis");
+
             InputController.Instance.MoveEvent += MoveEventListener;
-            _playerMesh = transform.Find("Capsule").gameObject;
-            _leftTurn = GameObject.Find("LeftTurnAxis").transform;
-            _rightTurn = GameObject.Find("RightTurnAxis").transform;
+        }
 
-            //EventRelayer.Instance.RequestMovePlayerEvent += MovePlayerRequestEventListener;
-            //EventRelayer.Instance.CameraChangeStartedEvent += CameraChangeStartedEventListener;
-            //EventRelayer.Instance.CameraChangeFinishedEvent += CameraChangeFinishedEventListener;
+        private void OnGUI()
+        {
+            var width = UnityEngine.Camera.main.pixelWidth;
+            var height = UnityEngine.Camera.main.pixelHeight;
+            Rect mainRect = new Rect(width * 0.2f, height * 0.04f, 300, 200);
 
-            //EventRelayer.Instance.MenuOpenedEvent += MenuOpenedEventListener;
-            //EventRelayer.Instance.MenuClosedEvent += MenuClosedEventListener;
+            GUIStyle fontStyle = GUI.skin.label;
+            fontStyle.fontSize = 20;
 
-            //EventRelayer.Instance.PlayerStateChangedEvent += PlayerStateChangedEventListener;
+            GUI.Box(mainRect, GUIContent.none);
+            GUI.Label(new Rect(mainRect.x + 5, mainRect.y, 250, 30), $"Horz {_horizontalInput} |Vert: {_verticalInput}", fontStyle);
+            GUI.Label(new Rect(mainRect.x + 5, mainRect.y + 30, 250, 30), $"Velocity: {_rigidbody.velocity}", fontStyle);
         }
 
         private void Update()
         {
-            //ProcessMovementInput();
-            ProcessMowerMovement();
+            
+            ProcessMovementInput();
+            //ProcessMowerMovement();
         }
 
         #region Event Listeners
@@ -53,47 +58,6 @@ namespace LawnCareSim.Player
         {
             _horizontalInput = e.x;
             _verticalInput = e.y;
-        }
-        /*
-        private void CameraChangeStartedEventListener(object sender, EventRelayer.CameraBlendData argse)
-        {
-            TogglePlayerControl(false);
-        }
-
-        private void CameraChangeFinishedEventListener(object sender, EventRelayer.CameraBlendData args)
-        {
-            TogglePlayerControl(true);
-        }
-        */
-
-        private void MovePlayerRequestEventListener(object sender, Transform args)
-        {
-            _characterController.enabled = false;
-            transform.position = args.position;
-            _characterController.enabled = true;
-        }
-
-        private void MenuOpenedEventListener(object sender, MenuName menu)
-        {
-            TogglePlayerControl(false);
-        }
-
-        private void MenuClosedEventListener(object sender, MenuName menu)
-        {
-            TogglePlayerControl(true);
-        }
-
-        private void PlayerStateChangedEventListener(object sender, PlayerState state)
-        {
-            switch(state)
-            {
-                case PlayerState.Sleeping:
-                    TogglePlayerControl(false);
-                    break;
-                default:
-                    TogglePlayerControl(true);
-                    break;
-            }
         }
         #endregion
     }
@@ -105,9 +69,6 @@ namespace LawnCareSim.Player
 
         private bool _canMove = true;
         private bool _isBeingMovedManually = false;
-        private bool _usingMower = true;
-        private bool _isOffsetRight;
-        private bool _isOffsetLeft;
 
         private Vector3 _moveDirection;
         private float _turnSmoothVelocity;
@@ -127,6 +88,7 @@ namespace LawnCareSim.Player
             }
         }
 
+        // Move Speed: 10
         private void ProcessMovementInput()
         {
             if (!_canMove || _isBeingMovedManually)
@@ -136,33 +98,25 @@ namespace LawnCareSim.Player
 
             Vector3 direction = new Vector3(_horizontalInput, 0f, _verticalInput).normalized;
 
-
             if (direction.magnitude >= 0.1f)
             {
                 float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
 
-                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, _smoothTurnTime);
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, 0.1f);
                 transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
                 _moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-                _characterController.Move(_moveDirection * _movementSpeed * UnityEngine.Time.deltaTime);
 
-                if (_currentSpeed != MovementSpeed.Walking)
-                {
-                    CurrentSpeed = MovementSpeed.Walking;
-                }
+                _rigidbody.velocity = _moveDirection * _movementSpeed;
             }
             else
             {
-                _characterController.Move(new Vector3(0, -9.8f * UnityEngine.Time.deltaTime, 0));
-
-                if (_currentSpeed != MovementSpeed.Idling)
-                {
-                    CurrentSpeed = MovementSpeed.Idling;
-                }
+                _rigidbody.velocity = Vector3.zero;
             }
         }
 
+        // Move Speed: 5
+        // Rotation Speed: 75
         private void ProcessMowerMovement()
         {
             if (!_canMove || _isBeingMovedManually)
@@ -172,130 +126,41 @@ namespace LawnCareSim.Player
 
             Vector3 direction = new Vector3(_horizontalInput, 0f, _verticalInput).normalized;
 
-            if (direction.magnitude >= 0.1f)
+            // Only move if forward or backward input is given
+            if (!MathHelpers.IsWithinRange(direction.z, 0f, 0.1f))
             {
-                //OffsetMesh(direction.x);
-
-                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
-                if (targetAngle < 0)
+                // Rotating Left
+                if (direction.x > 0.1f)
                 {
-                    targetAngle += 360;
+                    _rigidbody.velocity = Vector3.zero;
+                    transform.RotateAround(_rightTurnPivot.transform.position, direction.z * Vector3.up, _rotationSpeed * Time.deltaTime);
+                }
+                
+                // Rotating Right
+                else if (direction.x < -0.1f)
+                {
+                    _rigidbody.velocity = Vector3.zero;
+                    transform.RotateAround(_leftTurnPivot.transform.position, direction.z * -Vector3.up, _rotationSpeed * Time.deltaTime);
                 }
 
-                bool dontTurn = false;
-                // Currently turning
-                if (!MathHelpers.IsWithinRange(transform.rotation.eulerAngles.y, targetAngle, 1.0f))
+                // Otherwise move in forward direction
+                else
                 {
-                    if (!(direction.x != 0 && direction.z != 0))
-                    {
-                        dontTurn = true;
-                        //Trying to turn without moving forward. Disallow
-                        Debug.Log($"No movement turn: {transform.rotation.eulerAngles.y} | {targetAngle}");
-                    }
-                    else
-                    {
-                        dontTurn = false;
-                    }
+                    float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+                    _moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * transform.forward;
+
+                    _rigidbody.velocity = _moveDirection * _movementSpeed;
                 }
-
-                if (!dontTurn)
-                {
-                    _moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-                    _characterController.Move(_moveDirection * _movementSpeed * UnityEngine.Time.deltaTime);
-
-                    float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, _smoothTurnTime);
-                    transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
-                }
-
-                //transform.RotateAround(direction.x < 0 ? _leftTurn.position : _rightTurn.position, Vector3.up, targetAngle * Time.deltaTime);
-
-
-
             }
             else
             {
-                _characterController.Move(new Vector3(0, -9.8f * UnityEngine.Time.deltaTime, 0));
+                _rigidbody.velocity = Vector3.zero;
             }
         }
 
         private void TogglePlayerControl(bool enable)
         {
             _canMove = enable;
-            _characterController.enabled = enable;
-        }
-
-        public IEnumerator LerpToPoint(Transform destination)
-        {
-            _isBeingMovedManually = true;
-
-            Vector3 startPos = transform.position;
-            Vector3 startRot = transform.rotation.eulerAngles;
-
-            float elapsedTime = 0f;
-            float distance = Vector3.Distance(startPos, destination.position);
-            while (distance > 0.1f)
-            {
-                transform.position = Vector3.Lerp(startPos, destination.position, elapsedTime / 0.5f);
-                transform.eulerAngles = Vector3.Lerp(startRot, destination.eulerAngles, elapsedTime / 0.5f);
-
-                distance = Vector3.Distance(transform.position, destination.position);
-                elapsedTime += UnityEngine.Time.deltaTime;
-                yield return null;
-            }
-
-            transform.position = destination.position;
-            transform.eulerAngles = destination.eulerAngles;
-
-            _isBeingMovedManually = false;
-
-            yield return null;
-        }
-
-        public void ToggleMowerControls(bool enable)
-        {
-            if (enable)
-            {
-                _movementSpeed = 4.0f;
-                _smoothTurnTime = 0.3f;
-                _usingMower = true;
-            }
-            else
-            {
-               // ResetMesh();
-                _movementSpeed = 10.0f;
-                _smoothTurnTime = 0.1f;
-                _usingMower = false;
-            }
-        }
-
-        private void OffsetMesh(float horzInput)
-        {
-            if (!_usingMower)
-            {
-                return;
-            }
-
-            if (_isOffsetLeft && horzInput < 0)
-            {
-                return;
-            }
-
-            if (_isOffsetRight && horzInput > 0)
-            {
-                return;
-            }
-
-            _isOffsetLeft = horzInput < 0;
-            _isOffsetRight = horzInput > 0;
-
-            var startPos = _playerMesh.transform.localPosition;
-            _playerMesh.transform.localPosition = new Vector3(horzInput > 0 ? -0.5f : 0.5f, 0, 0);
-        }
-
-        private void ResetMesh()
-        {
-            _playerMesh.transform.localPosition = Vector3.zero;
         }
     }
 }
