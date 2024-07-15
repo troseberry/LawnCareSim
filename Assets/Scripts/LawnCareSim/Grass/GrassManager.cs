7d20b9d36aa4e0edccae6c6eba0d7d112548f7c7
troseberry <tayroseberry@gmail.com>
2024-07-14 23:41:26 -0700

    Updated GrassManager to spawn overgrown edges

--- Assets/Scripts/LawnCareSim/Grass/GrassManager.cs
+++ Assets/Scripts/LawnCareSim/Grass/GrassManager.cs
@@ -9,6 +9,7 @@ namespace LawnCareSim.Grass
         public Vector2 Location;
         public float Height;
         public bool WasCut;
+        public GameObject OvergrownEdge;
     }
 
     public partial class GrassManager : MonoBehaviour
@@ -48,6 +49,7 @@ namespace LawnCareSim.Grass
         private Dictionary<string, GrassObject> _grassObjects = new Dictionary<string, GrassObject>();
 
         [SerializeField] private GameObject _grassPrefab;
+        [SerializeField] private GameObject _overgrownGrassPrefab;
         [SerializeField] private Transform _grassParent;
         [SerializeField] private Vector2 _horizontalRange;
         [SerializeField] private Vector2 _verticalRange;
@@ -71,17 +73,55 @@ namespace LawnCareSim.Grass
             {
                 for (int j = 0; j < yRange * 2 + 1; j++)
                 {
+                    bool canSpawnEdge = i == 0 || i == xRange * 2 || j == 0 || j == yRange * 2;
+
                     Vector3 spawn = new Vector3(xMin + (i * 0.5f), 0.5f, yMin + (j * 0.5f));
                     var grass = Instantiate(_grassPrefab, spawn, Quaternion.identity, _grassParent);
                     grass.name = $"Grass_{grassCount}";
 
-                    _grassObjects.Add(grass.name, new GrassObject 
-                    { 
+                    var grassObj = new GrassObject
+                    {
                         GameObject = grass,
                         Location = spawn,
                         Height = grass.transform.localScale.y,
                         WasCut = false,
-                    });
+                    };
+
+                    if (canSpawnEdge)
+                    {
+                        if (Random.Range(0, 2) == 1)
+                        {
+                            Vector3 edgeSpawn = spawn;
+                            Quaternion edgeRotation = Quaternion.identity;
+
+                            if (i == 0)
+                            {
+                                edgeSpawn.x -= 1;
+                                edgeRotation = Quaternion.Euler(new Vector3(0f, 180f, 0f));
+                            } 
+                            else if (i == xRange * 2)
+                            {
+                                edgeSpawn.x += 1;
+                            }
+                            else if (j == 0)
+                            {
+                                edgeSpawn.z -= 1;
+                                edgeRotation = Quaternion.Euler(new Vector3(0f, 90f, 0f));
+                            }
+                            else if (j == yRange * 2)
+                            {
+                                edgeSpawn.z += 1;
+                                edgeRotation = Quaternion.Euler(new Vector3(0f, 270f, 0f));
+                            }
+
+                            var edge = Instantiate(_overgrownGrassPrefab, edgeSpawn, edgeRotation, grassObj.GameObject.transform);
+                            edge.name = "OvergrownEdge";
+
+                            grassObj.OvergrownEdge = edge;
+                        }
+                    }
+                    
+                    _grassObjects.Add(grass.name, grassObj);
 
                     grassCount++;
                     j++;
@@ -90,5 +130,12 @@ namespace LawnCareSim.Grass
                 i++;
             }
         }
+
+        /*
+        private GameObject SpawnEdge(GrassObject grassObj)
+        {
+
+        }
+        */
     }
 }
